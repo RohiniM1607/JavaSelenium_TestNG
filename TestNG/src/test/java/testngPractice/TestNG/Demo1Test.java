5a7116d3ddf0acafa176444afd096c12e783df79
RohiniM1607 <mrohini7558@gmail.com>
2026-04-23 21:56:47 +0530

    TestNG Practice Renamed

--- TestNG/src/test/java/testngPractice/TestNG/Demo1Test.java
+++ TestNG/src/test/java/testngPractice/TestNG/Demo1Test.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Demo1 {
+public class Demo1Test {
 	public WebDriver driver;
 	
 	@Test
